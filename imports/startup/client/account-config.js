5b5d1dc4beb1d71a2bb5828ef6d57a7533574d46
Iurii Bodoprostov <iuriibod@gmail.com>
2016-04-07 12:14:34 +0300

    deleted checkbox

--- imports/startup/client/account-config.js
+++ imports/startup/client/account-config.js
@@ -20,19 +20,5 @@ Accounts.ui.config({
     fieldLabel: 'Last name',
     inputType: 'text',
     visible: true,
-  }, {
-    fieldName: 'terms',
-    fieldLabel: 'I accept the terms and conditions',
-    inputType: 'checkbox',
-    visible: true,
-    saveToProfile: false,
-    validate: function(value, errorFunction) {
-      if (value) {
-        return true;
-      } else {
-        errorFunction('You must accept the terms and conditions.');
-        return false;
-      }
-    }
   }]
 });
