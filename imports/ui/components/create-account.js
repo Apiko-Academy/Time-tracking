32eeca6283762ca259a630ccc605159990e3c151
Iurii Bodoprostov <iuriibod@gmail.com>
2016-04-07 10:51:59 +0300

    formated code2

--- imports/ui/components/create-account.js
+++ imports/ui/components/create-account.js
@@ -1,23 +1,22 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.createUser = function(options, callback) {
-	options = _.clone(options); // we'll be modifying options
+  options = _.clone(options); // we'll be modifying options
 
-	if (typeof options.password !== 'string')
-	    throw new Error("options.password must be a string");
-	if (!options.password) {
-	    callback(new Meteor.Error(400, "Password may not be empty"));
-	    return;
-	}
+  if (typeof options.password !== 'string')
+    throw new Error("options.password must be a string");
+  if (!options.password) {
+    callback(new Meteor.Error(400, "Password may not be empty"));
+    return;
+  }
 
-	// Replace password with the hashed password.
-	options.password = Accounts._hashPassword(options.password);
-	options.profile.url = '';
+  // Replace password with the hashed password.
+  options.password = Accounts._hashPassword(options.password);
+  options.profile.url = '';
 
-	Accounts.callLoginMethod({
-	  methodName: 'createUser',
-	  methodArguments: [options],
-	  userCallback: callback
-	});
-
+  Accounts.callLoginMethod({
+    methodName: 'createUser',
+    methodArguments: [options],
+    userCallback: callback
+  });
 };
