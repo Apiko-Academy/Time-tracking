0804cfd4b34b8e9f70d73ced3c39766c42ebca89
Iurii Bodoprostov <iuriibod@gmail.com>
2016-04-11 15:11:28 +0300

    changed helper isUserInRole

--- imports/ui/components/ui-helpers.js
+++ imports/ui/components/ui-helpers.js
@@ -5,6 +5,10 @@ Template.registerHelper('isUserInRole', (role = 'owner', userId = Meteor.userId(
   if (typeof userId === "object") {
     userId = Meteor.userId();
   }
+  
+  if (typeof group === "string") {
+    return Roles.userIsInRole(userId, role, group);
+  }
 
   return Roles.userIsInRole(userId, role);
 });
