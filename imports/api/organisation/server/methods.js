6b781048c87eac7c2408c6a63507041ddd4df5cc
Nazarii <nazarii.strohush@gmail.com>
2016-05-06 10:29:38 +0300

    User acces to pages and methods

--- imports/api/organisation/server/methods.js
+++ imports/api/organisation/server/methods.js
@@ -37,7 +37,36 @@ Meteor.methods({
     return organisationId;
   },
 
+  editOrganisation: function (organisationData) {
+    check(organisationData, {
+      _id: MongoId,
+      description: String,
+      name: String,
+      profile: {
+        companySite: Match.Optional(String),
+        iconUrl: String
+      },
+      users: [MongoId],
+      owners: [MongoId]
+    });
+
+    if(!Roles.userIsInRole(this.userId, 'owner', organisationData._id)){ 
+      throw new Meteor.Error('You dont have permissions to edit this organisation'); 
+    }
+
+    _.each(organisationData.owners, function(organisationOwner) {
+      Roles.setUserRoles(organisationOwner, ['owner'], organisationData._id);
+
+      if (Roles.userIsInRole(organisationOwner, 'owner', 'general_group') && Roles.userIsInRole(organisationOwner, 'owner', organisationData._id)) {
+        Roles.removeUsersFromRoles(organisationOwner, ['owner'], 'general_group');
+      }
+    });
+    Organisation.update({_id: organisationData._id}, {$set: organisationData});
+  },
   addUsersToRoles: function(userId, role, organisationId) {
+    if(!Roles.userIsInRole(this.userId, 'owner', organisationData._id)){ 
+      throw new Meteor.Error('You dont have permissions to edit this organisation'); 
+    }
     Roles.addUsersToRoles(userId, role, organisationId);
     return true;
   }
