c53946e0d384bebf4a4dd3f37dc2b846fc60ef5f
Nazarii <nazarii.strohush@gmail.com>
2016-05-12 10:30:39 +0300

    Separated project sharing to Project_sharing template

--- imports/ui/components/project/edit/project-edit.js
+++ imports/ui/components/project/edit/project-edit.js
@@ -1,44 +1,4 @@
 import './project-edit.html';
-import '../../users-select/users-select.js';
-import './users-add.js';
-import '../../permissions-page/permissions.html';
+import './project-sharing.js';
 
-import 'meteor/trsdln:modals';
-
-import { getFullName, getProfileIcon, getUserEmail } from '../../../../modules/users.js';
-import { handleMethodResult } from '../../../../modules/handle-method-result.js';
-import { Meteor } from 'meteor/meteor';
-import { ReactiveArray } from 'meteor/manuel:reactivearray';
-import { Template } from 'meteor/templating';
-
-Template.Project_edit.helpers({
-  projectMembers(){
-    let project = Template.instance().data.project;
-    return [...project.managers, ...project.workers];
-  },
-  fullName(){
-  	return getFullName(this.toString());
-  },
-  email(){
-  	return getUserEmail(this.toString());
-  },
-  icon(){
-  	return getProfileIcon(this.toString());
-  }
-});
-
-Template.Project_edit.events({
-  'click .worker-set': function (event, tmpl){
-    event.preventDefault();
-    Meteor.call('project.member.set', tmpl.data.project._id, event.target.id, handleMethodResult());
-  },
-  'click .manager-set': function(event, tmpl){
-    event.preventDefault();
-    Meteor.call('project.member.set', tmpl.data.project._id, event.target.id, 'manager', handleMethodResult());  
-  },
-  'click .remove-member': function(event, tmpl){
-    event.preventDefault();
-    Meteor.call('project.member.remove', tmpl.data.project._id, event.target.id, handleMethodResult());  
-  }
-});
 
