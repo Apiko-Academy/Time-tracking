bda7670196df2c49f943f79614669b80897f2151
Nazarii <nazarii.strohush@gmail.com>
2016-05-12 09:49:57 +0300

    Added href to project list rows

--- imports/ui/components/project/list/projects-list.js
+++ imports/ui/components/project/list/projects-list.js
@@ -1,2 +1,11 @@
 import './projects-list.html';
 import './projects-list-item.js';
+
+import { Template } from 'meteor/templating';
+import { Router } from 'meteor/iron:router';
+
+Template.Projects_list.events({
+   'click .table-row': function(){
+     Router.go('projectSettings', {_id: this._id});
+   }
+});
