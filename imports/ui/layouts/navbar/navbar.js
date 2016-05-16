a9dfa08db20c970b5987a4f1d9910ea7617deb4f
Viktor Vyshnevskyi <victor.vyshnevsky64@gmail.com>
2016-05-16 10:01:03 +0300

    updated interface, removed unused packages

--- imports/ui/layouts/navbar/navbar.js
+++ imports/ui/layouts/navbar/navbar.js
@@ -1,11 +1,20 @@
-import './loginButtons.html';
 import './navbar.html';
 
 import '../topmenu/topmenu.js';
 import { Template } from 'meteor/templating';
+import { Router } from 'meteor/iron:router';
 
 Template.navbar.events({
 	'click .sign-out': function() {
 		Meteor.logout();
+	},
+	'click .sign-in': function() {
+		Router.go('signIn');
+	},
+	'click .sign-up': function() {
+		Router.go('signUp');
+	},
+	'click .forgot-password': function() {
+		Router.go('forgotPassword');
 	}
 });
